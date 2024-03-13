9198fad69b76e837ae76a44b2210bd7d1bb51c55
0lder0 <lilii-ann.older@khk.ee>
2024-03-14 01:20:15 +0200

    Getting started

---
+++ gallery/src/App.jsx
@@ -0,0 +1,11 @@
+import './app.scss'
+
+function App() {
+
+  return (
+    <div className='hello-scss'>Hello and Welcome
+      <div className='sub-hello'>Something new</div></div>
+  )
+}
+
+export default App
